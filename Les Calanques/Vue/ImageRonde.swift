d1b6dfa522b2207710102cf5725bf1057395581f
Matthieu <matthieupasserel@gmail.com>
2018-07-25 11:08:00 +0200

    Custom TableViewCell

---
+++ Les Calanques/Vue/ImageRonde.swift
@@ -0,0 +1,32 @@
+//
+//  ImageRonde.swift
+//  Les Calanques
+//
+//  Created by Matthieu PASSEREL on 25/07/2018.
+//  Copyright © 2018 Matthieu PASSEREL. All rights reserved.
+//
+
+import UIKit
+
+class ImageRonde: UIImageView {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setup()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setup()
+        
+    }
+    
+    func setup() {
+        layer.cornerRadius = frame.height / 2
+        layer.borderColor = UIColor.white.cgColor
+        layer.borderWidth = 2
+        clipsToBounds = true
+        contentMode = .scaleAspectFill
+    }
+
+}
